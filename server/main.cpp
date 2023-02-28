acfef68224a068c0bc0e81b9897031c0669ee4f1
yoakin <joaquin.delgado-carrascal@epitech.eu>
2023-02-28 22:48:11 +0100

    [UPD] compilation error fixed.

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,12 @@
 #include "TCPServer.hpp"
+using namespace Server;
 
 int main()
 {
     boost::asio::io_service io_service;
 
-    Server server(io_service);
+    TCPServer server(io_service);
+    server.start();
+
     return 0;
 }
